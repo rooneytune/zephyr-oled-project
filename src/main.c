91bf0af9838679c8620020bd8d92d62e2e55fad2
rooneytune <rooneysemail@gmail.com>
2024-02-22 22:14:45 +0000

    removed pin access from Click event.  Added Click count to display

--- src/main.c
+++ src/main.c
@@ -24,10 +24,8 @@ LOG_MODULE_REGISTER(main);
 #define LV_COLOR_WHITE 0xFFFFFF
 
 static lv_obj_t *temp_label, *temp_value_label;
-static lv_obj_t *hum_label, *hum_value_label;
-static lv_obj_t *pm25_label, *pm25_value_label;
-static lv_obj_t *voc_label, *voc_value_label;
-static char temp_value[11] = {0};
+static lv_obj_t *button_press_count_label, *button_press_count_value_label;
+
 
 // Declaration of all Device tree struct
 static const struct device *gpio_ct_dev =
@@ -38,6 +36,8 @@ static const struct gpio_dt_spec led =
     GPIO_DT_SPEC_GET(DT_NODELABEL(blinking_led), gpios);
 
 static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
+
+static volatile int button_press_count=0;
 
 /*
 Methods / types used by button events
@@ -50,7 +50,7 @@ static char *helper_button_evt_str(enum button_evt evt)
     return "Pressed";
   case BUTTON_EVT_RELEASED:
     return "Released";
-    displayHelloWorldMessage_CFB();
+    
   default:
     return "Unknown";
   }
@@ -58,11 +58,13 @@ static char *helper_button_evt_str(enum button_evt evt)
 
 static void button_event_handler(enum button_evt evt)
 {
-  printk("Button event: %s\n", helper_button_evt_str(evt));
+  button_press_count++;
+  printk("Button event: %s - %d\n", helper_button_evt_str(evt),button_press_count);
   switch (evt)
   {
   case BUTTON_EVT_PRESSED:
-    gpio_pin_toggle_dt(&led);
+    //gpio_pin_toggle_dt(&led);
+    
     break;
 
   default:
@@ -116,7 +118,7 @@ bool setupLeds()
 }
 
 // Basic steps, required for all display libraries
-bool setupDisplayDevice()
+static bool setupDisplayDevice()
 {
   printk("Initialising display !\n");
   if (display == NULL)
@@ -146,111 +148,21 @@ bool setupDisplayDevice()
   // LOG_INF("LV display components initialised");
   return true;
 };
-//-----------------------------------------------------------------------
-// Uses CharacterFrameBuffer to configure display and display a message
-// THIS WORKS !  So device tree setup must be OK
-//-----------------------------------------------------------------------
-// void displayHelloWorldMessage_CFB(void)
-// {
-//   int ret;
-//   ret = cfb_framebuffer_init(display);
-//   if (ret != 0)
-//   {
-//     printk("Could not initialise the display\n");
-//     LOG_ERR("Could not initialise the display");
-//     return false;
-//   }
 
-//   ret = cfb_framebuffer_invert(display);
 
-//   ret = cfb_print(display, "Display OK 1", 0, 0);
-//   if (ret != 0)
-//   {
-//     LOG_ERR("could not print to display");
-//     return;
-//   }
-
-//   ret = cfb_framebuffer_finalize(display);
-
-//   if (ret != 0)
-//   {
-//     printk("could not finalise!\n");
-//     LOG_ERR("could not finalise");
-//     return;
-//   }
-//   LOG_INF("Finalised display.\n");
-
-// }
-
-//------------------------------------------------------------------------------------------
-// Sample code, not in use
-//-------------------------------------------------------------------------------------------
-void displayHelloWorldWithCounterLoop_LVGL(void)
-{
-
-  int ret;
-  LOG_INF("Initialising LVLG code . . . \n");
-
-  uint32_t count = 0U;
-  char count_str[11] = {0};
-
-  lv_obj_t *hello_world_label;
-  lv_obj_t *count_label;
-
-  printk("LVLG - labels defined\n");
-
-  if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN))
-  {
-    lv_obj_t *hello_world_button;
-
-    hello_world_button = lv_btn_create(lv_scr_act());
-    lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
-    hello_world_label = lv_label_create(hello_world_button);
-    printk("LVLG - button defined\n");
-  }
-  else
-  {
-    hello_world_label = lv_label_create(lv_scr_act());
-  }
-
-  lv_label_set_text(hello_world_label, "Hello world!");
-  lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
-  printk("LVLG - set text\n");
-
-  count_label = lv_label_create(lv_scr_act());
-  lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
-
-  lv_task_handler();
-  display_blanking_off(display);
-
-  // printk("Entering display loop");
-  // while (1)
-  // { // Set the active screen pointer to screen1
-  //   lv_scr_load(screen1);
-  //   sprintf(count_str, "%d", count / 100U);
-  //   lv_label_set_text(count_label, count_str);
-  // }
-  // lv_task_handler();
-  // k_msleep(1000);
-  // ++count;
-};
 
 //------------------------------------------------------------------------------------------
 // Works, but maybe an error with memory allocation
 //-------------------------------------------------------------------------------------------
-void displayHelloWorldLabel_LVGL(void)
+ static void add_controls_to_display_LVGL(void)
 {
-  LOG_INF("displayHelloWorldLabel_LVGL started\n");
+  LOG_INF("add_controls_to_display_LVGL started\n");
 
-  //********Processing stalls here !***********
-  lv_init();
-  // size_t lvglMemory;
-  // lvglMemory = 180000;
   //  #define DRAW_BUF_SIZE  (128 * 64 / 10 * (LV_COLOR_DEPTH / 8))
   // uint32_t draw_buf[DRAW_BUF_SIZE / 4];
-  
+  lv_init();
+  // size_t lvglMemory = 180000;
   // lv_mem_alloc(lvglMemory);
-  
 
   LOG_INF("LVGL - init OK\n");
 
@@ -260,82 +172,51 @@ void displayHelloWorldLabel_LVGL(void)
 
   // lv_style_set_text_font(&stRoberto, &rob);
 
+  //Set counter controls
   temp_label = lv_label_create(lv_scr_act());
-  lv_label_set_text(temp_label, "Roon*Deluxe:");
+  lv_label_set_text(temp_label, "Counter:");
   lv_obj_align(temp_label, LV_ALIGN_TOP_LEFT, 0, 0);
   lv_obj_add_style(temp_label, &stSmall, 0);
 
   temp_value_label = lv_label_create(lv_scr_act());
   lv_label_set_text(temp_value_label, "*");
-  lv_obj_align(temp_value_label, LV_ALIGN_TOP_LEFT, 0, 14);
+  lv_obj_align(temp_value_label, LV_ALIGN_TOP_LEFT, 65, 0);
   lv_obj_add_style(temp_value_label, &stSmall, 0);
+
+
+  //NOT YET IMPLEMENTED
+  button_press_count_label = lv_label_create(lv_scr_act());
+  lv_label_set_text(button_press_count_label, "Btn Press Cnt:");
+  lv_obj_align(button_press_count_label, LV_ALIGN_TOP_LEFT, 0, 12);
+  lv_obj_add_style(button_press_count_label, &stSmall, 0);
+
+  button_press_count_value_label = lv_label_create(lv_scr_act());
+  lv_label_set_text(button_press_count_value_label, "*");
+  lv_obj_align(button_press_count_value_label, LV_ALIGN_TOP_LEFT, 0, 20);
+  lv_obj_add_style(button_press_count_value_label, &stSmall, 0);
 
   LOG_INF("LVGL - labels created\n");
 
   lv_task_handler();
 
-  //   // Create a screen
-  //   lv_obj_t * screen = lv_scr_act();
-
-  //   // Create a style for the background
-  //   static lv_style_t style_bg;
-  //   lv_style_init(&style_bg);
-  //   lv_style_set_bg_color(&style_bg, lv_color_black()); // Set background color to black
-
-  //   // Create a style for the text
-  //   static lv_style_t style_text;
-  //   lv_style_init(&style_text);
-  //   lv_style_set_text_color(&style_text,  lv_color_white()); // Set text color to white
-
-  //   // Create a label (text)
-  //   lv_obj_t * label = lv_label_create(screen);
-  //   lv_label_set_text(label, "Hello, World!");
-
-  //   // Apply the style to the label
-  //   lv_obj_add_style(label, LV_OBJ_DRAW_PART_RECTANGLE, &style_text);
-
-  //   // Apply the style to the screen
-  //   lv_obj_add_style(screen, LV_OBJ_DRAW_PART_RECTANGLE, &style_bg);
-
-  // lv_disp_t * lv_disp = lv_disp_get_default();
-  // lv_theme_default_init(lv_disp, lv_color_black(), lv_color_white(), 1, &lv_font_montserrat_14);
-  // Initialize the LVGL display driver
-  // lv_disp_drv_init(&display);
-
-  // lv_disp_draw_buf_t disp_buf;
-  // static lv_color_t buf[LV_HOR_RES_MAX * LV_VER_RES_MAX / 10]; // Declare a buffer for 1/10 screen size
-  // lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * LV_VER_RES_MAX / 10); // Adjust the size according to your display
-
-  // lv_init();
-  //  // Initialize the display
-  // display_blanking_off(display);
-
-  // Create your screens
-  //  lv_obj_t *screen1 = lv_obj_create(NULL);
-
-  // // Set the active screen pointer to screen1
-  // lv_scr_load(screen1);
-
-  // lv_obj_t *temp_label = lv_label_create(lv_scr_act());
-  // lv_label_set_text(temp_label, "T: (C)");
-  // lv_obj_align(temp_label, LV_ALIGN_TOP_MID, 0, 0);
-
-  // lv_obj_t *temp_value_label = lv_label_create(lv_scr_act());
-  // lv_label_set_text(temp_value_label, "*");
-  // lv_obj_align(temp_value_label, LV_ALIGN_TOP_LEFT, 0, 14);
-
-  // lv_task_handler();
-
-  /*Change the active screen's background color*/
-  // lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57), LV_PART_MAIN);
-
-  // /*Create a white label, set its text and align it to the center*/
-  // lv_obj_t *label = lv_label_create(lv_scr_act());
-  // lv_label_set_text(label, "Hello world");
-  // lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
-  // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
+static void updateDisplay(int loopIndex)
+{
+  char label_value[11] ;
+  // Update Label with loop index
+   snprintf(label_value, sizeof(label_value) - 1, "%d", loopIndex);
+  lv_label_set_text(temp_value_label, label_value);
 
+
+// Not implemented
+
+   snprintf(label_value, sizeof(label_value) - 1, "%d", button_press_count);
+  lv_label_set_text(button_press_count_value_label, label_value);
+
+
+  // Update LVGL tasks
+  lv_task_handler();
+}
 int main(void)
 {
 
@@ -351,7 +232,7 @@ int main(void)
 
   // k_msleep(2000);
 
-  displayHelloWorldLabel_LVGL();
+  add_controls_to_display_LVGL();
 
   // Test the display - This method runs OK
   // displayHelloWorldMessage_CFB();
@@ -361,23 +242,14 @@ int main(void)
   gpio_pin_set_dt(&led, 0);
   while (true)
   {
-  
+
     loopIndex++;
-    LOG_INF("Main loop - Iteration: %d...\n", loopIndex);
+    //LOG_INF("Main loop - Iteration: %d...\n", loopIndex);
+    updateDisplay(loopIndex);
 
-    //Update Label with loop index
-    snprintf(temp_value, sizeof(temp_value) - 1, "%d", loopIndex);
-    lv_label_set_text(temp_value_label, temp_value);
-
-    // Update LVGL tasks
-    lv_task_handler();
-    
-
-    //Toggle led
+    // Toggle led
     ret = gpio_pin_toggle_dt(&led);
-    
-    k_msleep(1000);
 
-
+    k_msleep(300);
   }
 }
